fe63d3a48d5401c2664c7f08fb8984641d5b0171
gonzam77 <gonzam77@gmial.com>
2025-06-29 14:26:57 -0300

    rename

--- android/app/src/main/java/com/rutina360/MainActivity.kt
+++ android/app/src/main/java/com/rutina360/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.miapp
+package com.rutina360
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "MiApp"
+  override fun getMainComponentName(): String = "Rutina360"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
